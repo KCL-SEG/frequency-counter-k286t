5b4e15cae9d4f54ced8ce6abab3fe1f55b4924bf
Mohamed Abdulrahman <99272688+k286t@users.noreply.github.com>
2022-10-02 11:09:15 +0000

    "Before counting, convert all integers to strings"

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,16 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    list = []
+    for k in items:
+        if isinstance(k,int):
+            list.append(str(k))
+        else:
+            list.append(k)           
+    for i in list:
+        count = 0
+        for j in list:
+            if i==j:
+                count+=1
+        frequencies[i] = count
     return frequencies
